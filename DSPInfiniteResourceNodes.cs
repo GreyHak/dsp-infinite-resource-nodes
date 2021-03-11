09eda2d0a31250c26c43e4fcdf8aca27da557fe8
GreyHak <479851+GreyHak@users.noreply.github.com>
2021-03-10 18:30:51 -0800

    Blank project

---
+++ DSPInfiniteResourceNodes.cs
@@ -0,0 +1,21 @@
+﻿//
+// Copyright (c) 2021, Aaron Shumate
+// All rights reserved.
+//
+// This source code is licensed under the BSD-style license found in the
+// LICENSE.txt file in the root directory of this source tree. 
+//
+// Dyson Sphere Program is developed by Youthcat Studio and published by Gamera Game.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSPInfiniteResourceNodes
+{
+    public class DSPInfiniteResourceNodes
+    {
+    }
+}
