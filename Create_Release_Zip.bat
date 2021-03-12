90769571e2ba04201841eb6f1bd303119d60cda4
GreyHak <479851+GreyHak@users.noreply.github.com>
2021-03-11 17:50:54 -0800

    Zip update to install to patchers folder

--- Create_Release_Zip.bat
+++ Create_Release_Zip.bat
@@ -1,4 +1,9 @@
 @ECHO OFF
 "%ProgramFiles%\7-Zip\7z" a ..\GreyHak-DSP_Infinite_Resource_Nodes-9.zip icon.png LICENSE.txt manifest.json README.md
 cd bin\Release
-"%ProgramFiles%\7-Zip\7z" u ..\..\..\GreyHak-DSP_Infinite_Resource_Nodes-9.zip DSPInfiniteResourceNodes.dll
+DEL patches\DSPInfiniteResourceNodes.dll
+MKDIR patchers
+COPY DSPInfiniteResourceNodes.dll patchers
+"%ProgramFiles%\7-Zip\7z" u ..\..\..\GreyHak-DSP_Infinite_Resource_Nodes-9.zip patchers\DSPInfiniteResourceNodes.dll
+DEL patchers\DSPInfiniteResourceNodes.dll
+RMDIR patchers
