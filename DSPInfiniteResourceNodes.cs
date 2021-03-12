93be166e4934c34917bc37256231c32d22381e50
GreyHak <479851+GreyHak@users.noreply.github.com>
2021-03-11 16:52:02 -0800

    Initial implementation

--- DSPInfiniteResourceNodes.cs
+++ DSPInfiniteResourceNodes.cs
@@ -9,35 +9,154 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using BepInEx;
 using BepInEx.Logging;
-using HarmonyLib;
-using UnityEngine;
-using UnityEngine.UI;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
 
 namespace DSPInfiniteResourceNodes
 {
-    [BepInPlugin(pluginGuid, pluginName, pluginVersion)]
-    [BepInProcess("DSPGAME.exe")]
-    public class DSPInfiniteResourceNodes : BaseUnityPlugin
+    public static class DSPInfiniteResourceNodes
     {
-        public const string pluginGuid = "greyhak.dysonsphereprogram.infiniteresourcenodes";
-        public const string pluginName = "DSP Infinite Resource Nodes";
-        public const string pluginVersion = "1.0.0";
-        new internal static ManualLogSource Logger;
-        Harmony harmony;
+        public static IEnumerable<string> TargetDLLs { get; } = new[] { "Assembly-CSharp.dll" };
+        public static ManualLogSource Logger = BepInEx.Logging.Logger.CreateLogSource("DSP Infinite Resource Nodes");
 
-        public void Awake()
+        public static void Patch(AssemblyDefinition assembly)
         {
-            Logger = base.Logger;  // "C:\Program Files (x86)\Steam\steamapps\common\Dyson Sphere Program\BepInEx\LogOutput.log"
+            if (assembly.MainModule != null)  // Assembly-CSharp.dll
+            {
+                TypeDefinition minerComponent = assembly.MainModule.GetType("MinerComponent");
+                foreach (MethodDefinition method in minerComponent.Methods)
+                {
+                    if (method.Name == "InternalUpdate")
+                    {
+                        /*for (int lineIdx = 0; lineIdx < method.Body.Instructions.Count; lineIdx++)
+                        {
+                            Console.WriteLine(lineIdx.ToString() + ": " + method.Body.Instructions[lineIdx].ToString());
+                        }*/
 
-            harmony = new Harmony(pluginGuid);
-            harmony.PatchAll(typeof(DSPInfiniteResourceNodes));
+                        const int instructionIdx_miner1 = 187;
+                        const int instructionIdx_miner2 = 213;
+                        const int instructionIdx_miner3 = 227;
 
-            Logger.LogInfo("Initialization complete.");
+                        if (method.Body.Instructions[instructionIdx_miner1].OpCode != OpCodes.Ldc_I4_1 ||
+                            method.Body.Instructions[instructionIdx_miner2].OpCode != OpCodes.Ldc_I4_1 ||
+                            method.Body.Instructions[instructionIdx_miner3].OpCode != OpCodes.Ldc_I4_1)
+                        {
+                            Logger.LogInfo(method.Body.Instructions[instructionIdx_miner1].ToString());
+                            Logger.LogInfo(method.Body.Instructions[instructionIdx_miner2].ToString());
+                            Logger.LogInfo(method.Body.Instructions[instructionIdx_miner3].ToString());
+
+                            Logger.LogError("ERROR: The Dyson Sphere Program appears to have been updated.  The Infinite Resource Nodes mod needs to be updated.  Until then miners will consume node resources.");
+                        }
+                        else
+                        {
+                            ILProcessor ilProcessor = method.Body.GetILProcessor();
+
+                            // veinPool[num2].amount = veinPool[num2].amount - 1;
+                            // 186: IL_01d5: ldfld System.Int32 VeinData::amount
+                            // 187: IL_01da: ldc.i4.1 <<-- Replace with ldc.i4.0
+                            // 188: IL_01db: sub
+                            // 189: IL_01dc: stfld System.Int32 VeinData::amount
+                            ilProcessor.Replace(ilProcessor.Body.Instructions[instructionIdx_miner1], Instruction.Create(OpCodes.Ldc_I4_0));
+
+                            // factory.planet.veinAmounts[(int)veinPool[num].type] -= 1L;
+                            // 204: IL_020b: callvirt PlanetData PlanetFactory::get_planet()
+                            // 205: IL_0210: ldfld System.Int64[] PlanetData::veinAmounts
+                            // 206: IL_0215: ldarg.2
+                            // 207: IL_0216: ldloc.1
+                            // 208: IL_0217: ldelema VeinData
+                            // 209: IL_021c: ldfld EVeinType VeinData::type
+                            // 210: IL_0221: ldelema System.Int64
+                            // 211: IL_0226: dup
+                            // 212: IL_0227: ldind.i8
+                            // 213: IL_0228: ldc.i4.1 <<-- Replace with ldc.i4.0
+                            // 214: IL_0229: conv.i8
+                            // 215: IL_022a: sub
+                            ilProcessor.Replace(ilProcessor.Body.Instructions[instructionIdx_miner2], Instruction.Create(OpCodes.Ldc_I4_0));
+
+                            // veinGroups[(int)groupIndex].amount = veinGroups[(int)groupIndex].amount - 1L;
+                            // 226: IL_0249: ldfld System.Int64 PlanetData/VeinGroup::amount
+                            // 227: IL_024e: ldc.i4.1 <<-- Replace with ldc.i4.0
+                            // 228: IL_024f: conv.i8
+                            // 229: IL_0250: sub
+                            // 230: IL_0251: stfld System.Int64 PlanetData/VeinGroup::amount
+                            ilProcessor.Replace(ilProcessor.Body.Instructions[instructionIdx_miner3], Instruction.Create(OpCodes.Ldc_I4_0));
+
+                            Logger.LogInfo("Infinite resources patch applied for miners.");
+                        }
+
+                        break;
+                    }
+                }
+
+                TypeDefinition playerAction_Mine = assembly.MainModule.GetType("PlayerAction_Mine");
+                foreach (MethodDefinition method in playerAction_Mine.Methods)
+                {
+                    if (method.Name == "GameTick")
+                    {
+                        /*for (int lineIdx = 0; lineIdx < method.Body.Instructions.Count; lineIdx++)
+                        {
+                            Console.WriteLine(lineIdx.ToString() + ": " + method.Body.Instructions[lineIdx].ToString());
+                        }*/
+
+                        const int instructionIdx_icarus1 = 446;
+                        const int instructionIdx_icarus2 = 499;
+                        const int instructionIdx_icarus3 = 511;
+
+                        if (method.Body.Instructions[instructionIdx_icarus1].OpCode != OpCodes.Sub ||
+                            method.Body.Instructions[instructionIdx_icarus2].OpCode != OpCodes.Sub ||
+                            method.Body.Instructions[instructionIdx_icarus3].OpCode != OpCodes.Sub)
+                        {
+                            Logger.LogInfo(method.Body.Instructions[instructionIdx_icarus1].ToString());
+                            Logger.LogInfo(method.Body.Instructions[instructionIdx_icarus2].ToString());
+                            Logger.LogInfo(method.Body.Instructions[instructionIdx_icarus3].ToString());
+
+                            Logger.LogError("ERROR: The Dyson Sphere Program appears to have been updated.  The Infinite Resource Nodes mod needs to be updated.  Until then Icarus will consume node resources.");
+                        }
+                        else
+                        {
+                            ILProcessor ilProcessor = method.Body.GetILProcessor();
+
+                            // Handles the node amount
+                            // veinPool[num13].amount = veinPool[num13].amount - num12;
+                            // 444: IL_052c: ldfld System.Int32 VeinData::amount
+                            // 445: IL_0531: ldloc.s V_24
+                            // 446: IL_0533: sub
+                            // 447: IL_0534: stfld System.Int32 VeinData::amount
+                            ilProcessor.Replace(ilProcessor.Body.Instructions[instructionIdx_icarus1], Instruction.Create(OpCodes.Pop));
+
+                            // Handles the planet's total
+                            // factory.planet.veinAmounts[(int)veinData.type] -= (long)num12;
+                            // 489: IL_05c6: ldloc.1
+                            // 490: IL_05c7: callvirt PlanetData PlanetFactory::get_planet()
+                            // 491: IL_05cc: ldfld System.Int64[] PlanetData::veinAmounts
+                            // 492: IL_05d1: ldloca.s V_22
+                            // 493: IL_05d3: ldfld EVeinType VeinData::type
+                            // 494: IL_05d8: ldelema System.Int64
+                            // 495: IL_05dd: dup
+                            // 496: IL_05de: ldind.i8
+                            // 497: IL_05df: ldloc.s V_24
+                            // 498: IL_05e1: conv.i8
+                            // 499: IL_05e2: sub
+                            // 500: IL_05e3: stind.i8
+                            // 501: IL_05e4: ldloc.1
+                            ilProcessor.Replace(ilProcessor.Body.Instructions[instructionIdx_icarus2], Instruction.Create(OpCodes.Pop));
+
+                            // Handles the total in the node group
+                            // veinGroups[(int)groupIndex].amount = veinGroups[(int)groupIndex].amount - (long)num12;
+                            // 508: IL_05fc: ldfld System.Int64 PlanetData/VeinGroup::amount
+                            // 509: IL_0601: ldloc.s V_24
+                            // 510: IL_0603: conv.i8
+                            // 511: IL_0604: sub
+                            // 512: IL_0605: stfld System.Int64 PlanetData/VeinGroup::amount
+                            ilProcessor.Replace(ilProcessor.Body.Instructions[instructionIdx_icarus3], Instruction.Create(OpCodes.Pop));
+
+                            Logger.LogInfo("Infinite resources patch applied for Icarus.");
+                        }
+                        break;
+                    }
+                }
+            }
         }
     }
 }
--- README.md
+++ README.md
@@ -1,5 +1,22 @@
 # Infinite Resource Nodes for Dyson Sphere Program
 
+**DSP Infinite Resource Nodes** is a mod for the Unity game Dyson Sphere Program developed by Youthcat Studio and published by Gamera Game.  The game is available on [here](https://store.steampowered.com/app/1366540/Dyson_Sphere_Program/).
+
+With this mod resource nodes will produce unlimited resources.  Tired of node resource running out, and having to move and rebalance your miners?  If so, this mod's for you.  With this mod resource node functionality will work more like [Satisfactory](https://www.satisfactorygame.com/).
+
+The number of resource nodes will remain unchanged.  The rate at which you can pull resources out of a node is still limited.  This mod does not and will not change these game features.
+
+If you like this mod, please click the thumbs up at the [top of the page](https://dsp.thunderstore.io/package/GreyHak/DSP_Infinite_Resource_Nodes/) (next to the Total rating).  That would be a nice thank you for me, and help other people to find a mod you enjoy.
+
+If you have issues with this mod, please report them on [GitHub](https://github.com/GreyHak/dsp-infinite-resource-nodes/issues).  I try to respond within 12 hours.    You can also contact me at GreyHak#2995 on the [DSP Modding](https://discord.gg/XxhyTNte) Discord #tech-support channel..
+
+## Installation
+This mod uses the BepInEx mod patch framework.  So BepInEx must be installed to use this mod.  Find details for installing BepInEx [in their user guide](https://bepinex.github.io/bepinex_docs/master/articles/user_guide/installation/index.html#installing-bepinex-1).  This mod was tested with BepInEx x64 5.4.5.0 and Dyson Sphere Program 0.6.16.5831 on Windows 10.
+
+To manually install this mod, add the `DSPInfiniteResourceNodes.dll` to your `%PROGRAMFILES(X86)%\Steam\steamapps\common\Dyson Sphere Program\BepInEx\patchers\` folder.
+
+This mod can also be installed using ebkr's [r2modman](https://dsp.thunderstore.io/package/ebkr/r2modman/) mod manager by clicking "Install with Mod Manager" on the [DSP Modding](https://dsp.thunderstore.io/package/GreyHak/DSP_Infinite_Resource_Nodes/) site.
+
 ## Open Source
 The source code for this mod is available for download, review and forking on GitHub [here](https://github.com/GreyHak/dsp-infinite-resource-nodes) under the BSD 3 clause license.
 
